e71176d698ce178d82a1e25a11740e5fb67f063a
Thube Ganesh <thubeganeshs@gmail.com>
2021-02-15 12:24:03 +0530

    #10... Add mat-tab

--- ClientApp/src/app/quiz/api-wrapper/quiz-detail-screen-api.service.ts
+++ ClientApp/src/app/quiz/api-wrapper/quiz-detail-screen-api.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { FormlyFieldConfig } from '@ngx-formly/core';
+import {QuizEnums} from '@module/serverside'
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizDetailScreenApiService {
 
-  constructor() { }
+public state={
+  options: Object.values(QuizEnums.QuestionType) as QuizEnums.QuestionType[]
+}
+
+  constructor( ) {}
 
   public getQuizDetailScreen() {
     const config: FormlyFieldConfig[] = [
@@ -19,7 +24,18 @@ export class QuizDetailScreenApiService {
           required: true,
           appearance: 'outline',
           disabled: true
-        },
+        }
+      },
+      {
+        key: 'subjectId',
+        type: 'input',
+        templateOptions:{
+          type: 'number',
+          label: 'SubjectId',
+          required: true,
+          appearance: 'outline',
+          
+        }
       },
       {
         key: 'complexcity',
@@ -29,9 +45,8 @@ export class QuizDetailScreenApiService {
           label: 'Complexcity',
           required: true,
           appearance: 'outline'
-        },
+        }
       },
-      //TODO:Ganesh and Ankita :- add new field Select 
       {
         key: 'topic',
         type: 'input',
@@ -43,16 +58,48 @@ export class QuizDetailScreenApiService {
         },
       },
       {
+        key: 'correctAnswerId',
+        type:'input',
+        templateOptions:{
+          type:'text',
+          label: 'Correct Answer Id',
+          required: true,
+          appearance: 'outline',
+        }
+      },
+      {
+        key: 'type',
+        type:'input',
+        templateOptions:{
+          type:'text',
+          label: 'Question Type',
+          required: true,
+          appearance: 'outline',
+          options : this.state.options
+        }
+      },
+      {
+        key: 'questionText',
+        type:'input',
+        templateOptions:{
+          type:'text',
+          label: 'Question text',
+          required: true,
+          appearance: 'outline',
+          
+        }
+      }, 
+      {
         key: 'isActive',
         type: 'checkbox',
         templateOptions: {
           label: 'Is Active',
           required: true,
-          appearance: 'outline'
-        },
-      }
+          // appearance: 'outline'
+        }
+      }, 
     ];
-
+    console.log(this.state.options)
     return config;
   }
 }
